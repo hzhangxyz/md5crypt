72aa7a9219f4d260ffe4e29765a555ff81729f8a
trumpet <zh19970205@126.com>
2016-09-10 10:10:47 +0800

    update

--- test5.cu
+++ test5.cu
@@ -294,6 +294,13 @@ __global__ void gate_hash(char* key, char* hash, char* buffer){
 }
 
 int main(){
+  char* hash_src;
+  unsigned int hash_src_max = 64;
+  unsigned int hash_src_len = 0;
+  char* dict_src;
+  unsigned int dict_src_max = 64
+  unsigned int dict_src_len = 0;
+  hash_src = (char *) malloc();
   char* key;
   char* salt;
   char* buffer;
