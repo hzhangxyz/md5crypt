ddb9cf9c2b3f3221d052b133ed4b78a672f21432
trumpet <zh19970205@126.com>
2016-09-17 08:47:00 +0800

    update

--- test42.cu
+++ test42.cu
@@ -37,7 +37,7 @@ __device__ __forceinline__ void md5_process_block (const void *buffer, size_t le
   ctx->total[0] += lolen;
   ctx->total[1] += (len >> 32) + (ctx->total[0] < lolen);
 
-  for(int cnt = 0;cnt<nwords/16;cnt++){
+  for(int cnt = 0; cnt < nwords/16; cnt++){
     unsigned int *cwp = correct_words;
     unsigned int A_save = A;
     unsigned int B_save = B;
